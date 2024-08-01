9e202a938f642f4de4fb5dd5c02d10058efba5fc
jlpach <jlpach@gmail.com>
2024-07-31 22:21:22 -0600

    agregando eventos y listeners

--- js/buscar.js
+++ js/buscar.js
@@ -1,41 +1,31 @@
 let cartStorage = localStorage.getItem("recetas")
 cartStorage = JSON.parse(cartStorage)
 
-console.log(cartStorage)
-
-for (const item of cartStorage) {
-    console.log(item.titulo)
-}
-
 let cardContainer = document.getElementById("cardRecetaBuscar")
 
 function renderRecetasBuscador(cardItems) {
-    for (const receta of cardItems) {
+    console.log(cardItems.length)
+    if (cardItems.length === 0 || cardItems.length === undefined) {
         const card = document.createElement("div")
 
-        card.innerHTML = `<div class="card text-center mb-3" style = "width: 18rem;" >
+        card.innerHTML = `<a href="./pages/crear.html" rel="noopener noreferrer">Crear Recetas</a>`
+
+        cardContainer.appendChild(card)
+    } else {
+        for (const receta of cardItems) {
+            const card = document.createElement("div")
+
+            card.innerHTML = `<div class="card text-center mb-3" style = "width: 18rem;" >
                                 <div class="card-body">
                                     <h5 class="card-title">${receta.titulo}</h5>
                                     <p class="card-text">${receta.elaboracion}</p>
                                     <a href="#" class="btn btn-primary">Detalles</a>
                                 </div>
-                          </div >`
+                        </div >`
 
-        cardContainer.appendChild(card)
-    }
-    /*     cardItems.forEach(receta => {
-            const card = document.createElement("div")
-    
-            card.innerHTML = `<div class="card text-center mb-3" style = "width: 18rem;" >
-                                    <div class="card-body">
-                                        <h5 class="card-title">${receta.titulo}</h5>
-                                        <p class="card-text">${receta.elaboracion}</p>
-                                        <a href="#" class="btn btn-primary">Detalles</a>
-                                    </div>
-                              </div >`
-    
             cardContainer.appendChild(card)
-        }) */
+        }
+    }
 }
 
 renderRecetasBuscador(cartStorage)
